bd3beb34ed3f32039e6a45789a08f64876d83abf
SofiiaNykon <sofianikonovic@gmail.com>
2024-06-06 23:09:24 +0200

    added js function

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const input = document.getElementById("input-task");
 const button = document.getElementById("btn");
 const list = document.querySelector(".todo-list");
 
+button.addEventListener(`click`, () => {
+    const li = document.createElement("li");
+    const deleteBtn = document.createElement("button");
+    li.innerText = input.value;
+    li.className = "todo-list-item"
+    list.appendChild(li);
+    li.appendChild(deleteBtn);
+    deleteBtn.innerText ="Delete"
+
+})
